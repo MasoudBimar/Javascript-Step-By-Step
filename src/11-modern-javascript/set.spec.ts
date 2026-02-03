bec79bb052a38e100bf4b7612210c2e7f619070c
Masoud Bimmar <masoudbimar@gmail.com>
2026-02-03 20:53:03 +0100

    Set Covered

--- src/11-modern-javascript/set.spec.ts
+++ src/11-modern-javascript/set.spec.ts
@@ -56,4 +56,87 @@ describe('Set', () => {
 
         expect(set.size).toBe(2);
     });
+
+    it("should be iterable through foreach", () => {
+        let set = new Set();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+
+        let iterationCount = 0;
+
+        set.forEach(item => iterationCount++);
+
+        expect(iterationCount).toBe(3);
+    });
+
+    it("should be iterable through for-of", () => {
+        let set = new Set();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+
+        let iterationCount = 0;
+
+        for (const element of set) {
+            iterationCount++;
+        }
+
+        expect(iterationCount).toBe(3);
+    });
+
+    it("should return an iterator of arrays when enteries called", () => {
+        let set = new Set();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+
+        let entries = set.entries();
+        // ? entries = { [ 1, 1 ], [ 2, 2 ], [ 3, 3 ] }
+
+        let firstEntry = entries.next().value;
+
+        expect(firstEntry?.[0]).toBe(1);
+        expect(firstEntry?.[1]).toBe(1);
+    });
+
+    it("should return an iterator of values when values is called", () => {
+        let set = new Set();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+
+        let values = set.values();
+        //? values = { 1, 2, 3 }
+
+        let firstValue = values.next().value;
+
+        expect(firstValue).toBe(1);
+    });
+
+    it("should be able to be construted with an iterator (values)", () => {
+        let set = new Set();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+
+
+        let set2 = new Set(set.values());
+        expect(set2.size).toBe(3);
+    });
+
+    it("should be able to be construted with an iterator (set)", () => {
+        let set = new Set();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+
+
+        let set2 = new Set(set);
+        expect(set2.size).toBe(3);
+        expect(set2.has(1)).toBeTruthy();
+        expect(set2.has(2)).toBeTruthy();
+        expect(set2.has(3)).toBeTruthy();
+
+    });
 })
